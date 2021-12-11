325d495eea07b40d5e33d4b8e50f4d2eb1e8df92
unknown <macedoborges@gmail.com>
2021-12-11 01:12:28 -0300

    Ajustado a pagina 2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import { useState } from 'react';
 
 function Home() {
-    return <h1>Pagina Incial</h1>
+    return (
+        <div>
+            <h2>Home 2</h2>
+            <Contador />
+        </div>   
+    )
 }
 
+function Contador() {
+
+    const [contador,setContador]= useState(1);
+
+    function adiconarContador() {
+        setContador(contador + 1);
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adiconarContador}>Adicionar</button>
+        </div>
+    )
+
+}
 export default Home
 
 
